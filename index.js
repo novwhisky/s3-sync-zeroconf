ab0df83fa014831f9060d9bba4495e355d27c99e
Nathan Witt <wittnl@gmail.com>
2014-08-09 11:34:45 -0500

    Directory cascading

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var File = require("./lib/File.js"),
     Directory = require("./lib/Directory.js");
 
+var util = require("util");
+
+function log(o) {
+    console.log(util.inspect(o, true, 40));
+}
+
 
 var d = new Directory("FooDir");
+var e = new Directory("BarDir");
+var f = new File("Foo");
 
-console.log(d);
+d.add(e);
+e.add(f);
+
+log(f);
